d772f794ceefd20d94f1c4ac68eaf3182144a872
Hyper-hi <63490949+Hyper-hi@users.noreply.github.com>
2020-08-11 03:04:17 +0530

    Update img.py

--- userbot/plugins/img.py
+++ userbot/plugins/img.py
@@ -29,7 +29,7 @@ async def img_sampler(event):
         lim = lim.replace("lim=", "")
         query = query.replace("lim=" + lim[0], "")
     except IndexError:
-        lim = 100
+        lim = 60
     response = googleimagesdownload()
 
     # creating list of arguments
